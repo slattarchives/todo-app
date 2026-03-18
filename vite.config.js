c7abb9dc2f76318a77ecc2d69ccf3be573a3d9c7
https://git.21-school.ru/andersoc <olga@DESKTOP-C9CBEUU.localdomain>
2026-03-18 14:32:02 +0300

    add everything

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({
+ plugins: [react()],
+ base: '/todo-app/' // тут название репозитория
+})
